f469022aa4c7efde2f158f08afa47c3ba72df622
faouziha <fhaourigui1@gmail.com>
2023-06-28 21:08:48 +0100

    todolist first try

---
+++ app.js
@@ -0,0 +1,54 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+const items = ["Buy Food", "Cook Foode", "Eat Food"];
+const workItems = [];
+
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(express.static("public"))
+
+app.get("/", function (req, res) {
+
+let day = date.getDate()
+
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
+    });
+});
+
+
+app.post("/", function (req, res) {
+    const item = req.body.newItem;
+    if (req.body.list === "Work List") {
+        workItems.push(item)
+        res.redirect("/work")
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+})
+
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+})
+
+app.get("/about", function (req, res) {
+    res.render("about");
+})
+
+app.listen(5000, function () {
+    console.log('server started');
+});
